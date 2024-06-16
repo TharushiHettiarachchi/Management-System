6089fd4b6c51a49288820f427be5702a21b3fcaa
Tharushi Hettiarachchi <130998790+TharushiHettiarachchi@users.noreply.github.com>
2024-06-16 09:37:10 +0530

    sign in desigm

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Management System</title>
 </head>
 <body>
-    
+    <div class="signin-background">
+        <div class="signin-background-section1"></div>
+        <div class="signin-background-section2"></div>
+    </div>
 </body>
 </html>
