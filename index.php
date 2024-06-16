037eca850eb6c7b598fa7fc15f798924adc27542
Tharushi Hettiarachchi <130998790+TharushiHettiarachchi@users.noreply.github.com>
2024-06-16 13:12:19 +0530

    login/create account design completed

--- index.php
+++ index.php
@@ -3,12 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Management System</title>
 </head>
 <body>
     <div class="signin-background">
         <div class="signin-background-section1"></div>
-        <div class="signin-background-section2"></div>
+        <div class="signin-background-section2">
+            <div class="signin-title">Login</div>
+            <div class="signin-inputset">
+                <label for="" class="signin-label">Mobile Number</label>
+                <input type="text" class="signin-input"/>
+            </div>
+            <div class="signin-inputset">
+                <label for="" class="signin-label">Password</label>
+                <input type="text" class="signin-input"/>
+            </div>
+            <div class="signin-button-panel">
+                <button class="signin-button">Login</button>
+                <span class="create-account-label">Create Account</span>
+            </div>
+        </div>
     </div>
 </body>
 </html>
